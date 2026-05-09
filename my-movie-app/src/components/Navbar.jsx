8dae35cebe7f5dee7b2958f22235a06b2d192edf
Zizou10 <paolo.pilati93@gmail.com>
2026-05-09 10:51:27 +0200

    first commit

---
+++ my-movie-app/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import { NavLink } from "react-router";
+
+export default function Navbar () {
+    return (
+    <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+        <div className="container">
+            <div className="navbar-brand">
+            <NavLink to="/">
+                <img className="img-fluid w-25 rounded" src="https://fakestoreapi.com/icons/logo.png" alt="logo" />
+            </NavLink> 
+            </div>
+            <div className="navbar-nav">
+                   
+                <NavLink to="/" end className="nav-link">Home</NavLink>
+                <NavLink to="/movie" end className="nav-link">MovieDetails</NavLink>
+
+            </div>
+        </div>
+    </nav>
+    )
+}
