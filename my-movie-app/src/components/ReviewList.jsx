ee2d5ea77820f7dbab5cf801830dd0dbb5cdfd44
Zizou10 <paolo.pilati93@gmail.com>
2026-05-13 00:27:50 +0200

    Implementazione pagina MovieDetails (con aggiunta di ReviewList -> ReviewItem) TODO: render immagini locandina

--- my-movie-app/src/components/ReviewList.jsx
+++ my-movie-app/src/components/ReviewList.jsx
@@ -3,8 +3,8 @@ import ReviewItem from "./ReviewItem";
 export default function ReviewList({ reviews = [] }) {
   return (
     <div>
-      {reviews.length === 0 ? (<p>No reviews</p>) : (reviews.map(r => (
-          <ReviewItem key={r.id} review={r} />
+      {reviews.length === 0 ? (<p>No reviews</p>) : (reviews.map(review => (
+          <ReviewItem key={review.id} review={review}/>
         ))
       )}
     </div>
--- my-movie-app/src/pages/MovieDetails.jsx
+++ my-movie-app/src/pages/MovieDetails.jsx
@@ -1,7 +1,43 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import axios from "axios";
+
+import ReviewList from "../components/ReviewList";
+
 export default function MovieDetails() {
+
+  const { id } = useParams();
+
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3000/movies/${id}`)
+      .then((res) => {setMovie(res.data);})
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [id]);
+
+  if (!movie) {
+    return <h2>Loading...</h2>;
+  }
+
   return (
-    <div className="container">
-      <h1>Movie Details</h1>
+    <div className="container py-4">
+      <h1>{movie.title}</h1>
+      <p>
+        Director: {movie.director}
+      </p>
+      <p>
+        Genre: {movie.genre}
+      </p>
+      <p>
+        {movie.abstract}
+      </p>
+      <h3>Reviews</h3>
+      <ReviewList reviews={movie.reviews} />
     </div>
   );
 }
